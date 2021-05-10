41defd9d64dae3e6445f241f6f8e5b4e3ed0d954
Mg1055 <62701211+Mg1055@users.noreply.github.com>
2021-05-10 11:25:58 +0530

    Add files via upload

---
+++ Revstring.cpp
@@ -0,0 +1,44 @@
+#include<bits/stdc++.h>
+using namespace std;
+  
+// Returns true if x is an aplhabatic
+// character, false otherwise
+bool isAlphabet(char x)
+{
+    return ( (x >= 'A' && x <= 'Z') ||
+            (x >= 'a' && x <= 'z') );
+}
+  
+void reverse(char str[])
+{
+    // Initialize left and right pointers
+    int r = strlen(str) - 1, l = 0;
+  
+    // Traverse string from both ends until
+    // 'l' and 'r'
+    while (l < r)
+    {
+        // Ignore special characters
+        if (!isAlphabet(str[l]))
+            l++;
+        else if(!isAlphabet(str[r]))
+            r--;
+  
+        else // Both str[l] and str[r] are not spacial
+        {
+            swap(str[l], str[r]);
+            l++;
+            r--;
+        }
+    }
+}
+  
+
+int main()
+{
+    char str[] = "YUSS@NOOO!!EHy";
+    cout << "Input string: " << str << endl;
+    reverse(str);
+    cout << "Output string: " << str << endl;
+    return 0;
+}
